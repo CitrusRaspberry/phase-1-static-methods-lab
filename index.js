7c2a4a28f690e7e836756caf02d44a6b01f95c60
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2021-11-20 22:07:10 -0600

    commit tree 2a8b614bad0715bbcf572c6b04aa03b18a88b604

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'-\s]+/g, "");
+  }
+  static titleize(string) {
+    const arr = string.split(" ");
+    const exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let newArr = arr.map(word => {
+      if (exceptions.indexOf(word.toLowerCase()) < 0) {
+        return word[0].toUpperCase() + word.slice(1);
+      } else {
+        return word;
+      }
+    })
+    newArr[0] = newArr[0][0].toUpperCase() + newArr[0].slice(1);
+    return newArr.join(" ");
+  }
 }
